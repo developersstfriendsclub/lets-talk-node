14a1a97dbbf5ad7ea09d0207b1ee38369567c0e5
ambujchand <ambuj@geoalgo.com>
2025-08-20 01:37:39 +0530

    fixed some changes

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -4,12 +4,27 @@ import { Server as SocketIOServer } from 'socket.io';
 import { registerSocketHandlers } from './handlers';
 
 export const initSocketServer = (server: HTTPServer) => {
+  // const io = new SocketIOServer(server, {
+  //   cors: {
+  //     origin: "*",
+  //     methods: ["GET", "POST"]
+  //   }
+  // });
+
   const io = new SocketIOServer(server, {
+    path: '/socket.io',
+    transports: ['websocket', 'polling'],
     cors: {
-      origin: "*",
-      methods: ["GET", "POST"]
-    }
+      origin: [
+        'https://clientfriendclub.com',
+        'https://*.clientfriendclub.com',
+      ],
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type','Authorization'],
+    },
   });
+
 
   io.on("connection", (socket) => {
     console.log("Socket connected:", socket.id);
