483411183abb851a14a5e6566e8660100b84d2e3
Aditya <adityabasak7031@gmail.com>
2025-08-25 12:57:45 +0530

    add time and total route

--- src/routes/payments.route.ts
+++ src/routes/payments.route.ts
@@ -2,12 +2,14 @@
 
 import { Router } from "express";
 import { verifyToken } from "../middleware/auth.middleware";
-import { createOrder, verifyPayment } from "../controllers/payment.controller";
+import { createOrder, getTotalEarnedTime, getTotalPayments, verifyPayment } from "../controllers/payment.controller";
 
 const router = Router();
 
 router.post('/create-order', verifyToken, createOrder);
 router.post('/verify', verifyToken, verifyPayment);
+router.get('/time', verifyToken, getTotalEarnedTime);
+router.get('/total', verifyToken, getTotalPayments);
 
 // NOTE: webhook is NOT defined here — it is mounted in app.ts with express.raw()
 
